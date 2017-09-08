9515cbfcbb1c7c15ad0a08df50c3d2ef43928fd0
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-08 16:53:03 -0400

    Update views.py

--- python/project/views.py
+++ python/project/views.py
@@ -3,4 +3,5 @@ from project.models import Company
 
 @app.route("/")
 def hello():
+    company_no = Company.get(Company.company_no == 10)
     return "<span style='color:red'>Hello from Flask!</span>"
