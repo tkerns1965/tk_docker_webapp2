09ebeb50e4e691f761bca0a203cd52cb4bbdbe7d
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-09 05:57:40 -0400

    Update views.py

--- python/project/views.py
+++ python/project/views.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from project import app
 from project.models import Company
 
 @app.route("/")
 def hello():
     company_no = Company.get(Company.company_no == 10)
-    return "<span style='color:red'>Hello from Flask!</span>"
+    return render_template("show_company.html", company_no=company_no)
