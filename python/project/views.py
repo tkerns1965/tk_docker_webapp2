0e267b24dc308a16f3fd2be283e1b4e653a0b13d
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-09 11:40:54 -0400

    Update views.py

--- python/project/views.py
+++ python/project/views.py
@@ -3,6 +3,7 @@ from project import app
 from project.models import Company
 
 @app.route("/")
-def hello():
-    company_no = Company.get(Company.company_no == 10)
+def show_company():
+    # company_no = Company.get(Company.company_no == 10)
+    company_no = 11
     return render_template("show_company.html", company_no=company_no)
