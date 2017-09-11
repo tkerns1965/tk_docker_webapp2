0f542297f38214e302878f317f0e8bf1fa015e7e
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-10 22:00:25 -0400

    Update views.py

--- python/project/views.py
+++ python/project/views.py
@@ -4,6 +4,9 @@ from project.models import Company
 
 @app.route("/")
 def show_company():
+    user = {'nickname': 'Tony'}
     # company_no = Company.get(Company.company_no == 10)
-    company_no = 11
-    return render_template("show_company.html", company_no=company_no)
+    # company_no = 11
+    return render_template("show_company.html",
+                           title="Home"
+                           user=user)
