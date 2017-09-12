87e124889027b0a041f738c8d289b45204d82f9a
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-12 09:40:04 -0400

    Update views.py

--- python/project/views.py
+++ python/project/views.py
@@ -1,4 +1,5 @@
 from flask import render_template
+
 from project import app
 from project.models.tables import Company
 
@@ -6,7 +7,7 @@ from project.models.tables import Company
 def show_company():
     user = {'nickname': 'Tony'}
     company = Company.get(Company.company_no == 10)
-    return render_template("show_company.html",
-                           title="Home",
+    return render_template('show_company.html',
+                           title='Home',
                            user=user,
                            company=company)
