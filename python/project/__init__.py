b9e587b20e145c5a97780be46c4d9bef6c4ef90f
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-12 08:54:42 -0400

    Update __init__.py

--- python/project/__init__.py
+++ python/project/__init__.py
@@ -5,5 +5,3 @@ app = Flask(__name__)
 from peewee import MySQLDatabase
 
 database = MySQLDatabase('timesheets', **{'unix_socket': '/mariadb/socket/mariadb.sock', 'user': 'tony', 'password': '2hard4u'})
-
-# import project.views
